4baa88bdb618abf2a14bb48428b28806d22cbf88
Claire Riegger <jonfieldthe2nd@gmail.com>
2019-02-17 20:39:56 -0500

    Create gameClock.js

---
+++ src/gameClock.js
@@ -0,0 +1,29 @@
+//// Game Clock
+////     The function that carries out the invocation of all the necessary game and rendering code
+////
+
+var gameClock = {};
+
+gameClock.FPS = 30; gameClock.DT = 1000 / FPS;
+gameClock.run = function () {
+	Keys.update();
+    Math4D.runRotations();
+    Box.updateAll();
+	glRender();
+};
+
+gameClock.start = function () {
+    if (gameClock.__ID__ === null) {
+        gameClock.__ID__ = setInterval(gameClock.run, gameClock.DT);
+    } else {
+        //throw new Error("Attempted to start game clock when it was already running");
+    }
+};
+
+gameClock.stop = function () {
+    if (gameClock.__ID__ !== null) {
+        clearInterval(gameClock.__ID__);
+    } else {
+        //throw new Error("Attempted to stop a nonexistent game clock");
+    }
+}
