a2329d883e4e2d6068a39b07b5a7b6cd71e6933b
Claire Riegger <jonfieldthe2nd@gmail.com>
2019-02-17 20:30:45 -0500

    Create keyCheck.js

---
+++ src/keyCheck.js
@@ -0,0 +1,51 @@
+//// Keys
+////     A set of function that help to interface the game with keyboard presses
+////
+
+var Keys = {};
+Keys.pressed  = []; // A list of all currently pressed keys
+Keys.buffered = []; // A buffered key will stay in this list until 
+onkeydown = function (e) {
+	if (!Keys.pressed .includes(e.key)) { Keys.pressed .push(e.key);  }
+	if (!Keys.buffered.includes(e.key)) { Keys.buffered.push(e.key); }
+};
+onkeyup = function (e) {
+	if (Keys.pressed.includes(e.key)) {
+    	Keys.pressed.splice(Keys.pressed.indexOf(e.key), 1);
+    }
+};
+
+Keys.checkPressed = function (key) {
+    return Keys.pressed.includes(key);
+};
+Keys.checkBuffered = function (key) {
+    let ind = Keys.buffered.indexOf(key);
+    if (ind === -1) {
+        return false;
+    } else {
+    	Keys.buffered.splice(ind, 1);
+        return true;
+    }
+};
+
+Keys.update = function () {
+	if (Math4D.cam.transxz < 1 && Keys.checkPress("e")) {
+    	Math4D.cam.transxz = 1;
+    } else if (Math4D.cam.transxz > -1 && Keys.checkPress("q")) {
+        if (Math4D.cam.transxz === 0) {
+        	Math4D.cam.axz = 1;
+        	Math4D.rotate90xz(true);
+        }
+    	Math4D.cam.transxz = -1;
+    }
+    
+    if (Math4D.cam.transxzw < 1 && Keys.checkPress("1")) {
+    	Math4D.cam.transxzw = 1;
+    } else if (Math4D.cam.transxzw > -1 && Keys.checkPress("3")) {
+    	if (Math4D.cam.transxzw === 0) {
+        	Math4D.cam.axzw = 1;
+        	Math4D.rotate120xzw(true);
+        }
+    	Math4D.cam.transxzw = -1;
+    }
+}
